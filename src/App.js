134b3d717939e501f9de3f2c8bea46cb44907d4a
Draksharapu Maniteja <119885698+manitejadraksharapu@users.noreply.github.com>
2024-12-18 12:38:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TaskList from './Components/TaskList'
+
+function App() {
+  document.title = "Todo App"
+  return (
+    <div className="App">
+      <TaskList/>
+    </div>
+  );
+}
+
+export default App;
